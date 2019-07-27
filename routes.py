43be25aa598923f037975c4d8a48735f3fa5c528
Kwoktopus <z5161179@student.unsw.edu.au>
2019-07-27 13:56:59 +1000

    react website added

--- routes.py
+++ routes.py
@@ -1,37 +1,20 @@
-#!/usr/bin/python3
+from flask import flash, redirect, render_template, request, url_for, Flask
+import app.application
 
-
-# import os
-# from flask import Flask
-
-
-# app = Flask(__name__)
-# app.config.from_object(os.environ['APP_SETTINGS'])
-
-
-# @app.route('/')
-# def hello():
-#     return "Hello World!"
-
-
-# @app.route('/<name>')
-# def hello_name(name):
-#     return "Hello {}!".format(name)
-
-# if __name__ == '__main__':
-#     app.run()
-from flask import Flask
-app = Flask(__name__)
-
+a = app.application.app
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return 'home'
 
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+
+# def run():
+# 	app.run()
