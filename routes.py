c1419a6bcd4f5d3244139bd74ae1c5150b4bce82
z5165183 <staticdew@gmail.com>
2019-07-27 18:47:18 +1000

    jgasd

--- routes.py
+++ routes.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-	return render_template('home.html')
+    if request.method == 'POST':
+        # if request.form['u_name] == 'Do Something':
+        #     print("asdfasdf")
+        #     return return_template("garden.html")
+        # elif request.form['submit_button'] == 'Do Something Else':
+
+        #     return "2"
+        amount = request.form["amount"]
+        # iq = request.form["iq"]
+        print(amount + "POST REQUEST")
+
+
+    return render_template('home.html')
 
 
 
